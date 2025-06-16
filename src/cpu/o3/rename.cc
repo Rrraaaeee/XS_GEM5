8684a4c0630b5363a2aab19bf8501ffc4b2f946f
qingxuan <qingxuan.kang@starfivetech.com>
2025-06-16 12:31:29 +0800

    Enable rcvg reuse (and transitive reuse

--- src/cpu/o3/rename.cc
+++ src/cpu/o3/rename.cc
@@ -1422,12 +1422,13 @@ Rename::renameDestRegs(const DynInstPtr &inst, ThreadID tid)
         bool can_reuse = flat_dest_regid.isRenameable() && (flat_dest_regid.classValue()==IntRegClass ||
                          flat_dest_regid.classValue()==FloatRegClass);
 
-        // if (inst->rcvgValid()) {
-            // assert(can_reuse);
-            // old_rgid = map->lookupRgid(dest_reg);
-            // rgid = inst->dst_rgids[0];
-        // } else if (can_reuse) {
-        if (can_reuse) {
+        if (inst->rcvgValid()) {
+            // comment out this if-statement to disable transitive reuse
+            assert(can_reuse);
+            old_rgid = map->lookupRgid(dest_reg);
+            rgid = inst->dst_rgids[0];
+        } else if (can_reuse) {
+        // if (can_reuse) {
             // get new rgid
             int flat_reg_idx = flat_dest_regid.classValue() * 32 + flat_dest_regid.index();
             assert(flat_reg_idx >=0 && flat_reg_idx <64);
